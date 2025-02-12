1588d9471b5f76fb99c2fb980e4d15cc2fd1b213
tepani <stephanieomongos05@gmail.com>
2025-02-12 09:42:21 +0800

    Added HelloWorld class

---
+++ Lab_act1_arithmitics/src/main/java/com/mycompany/lab_act1_arithmitics/Lab_act1_arithmitics.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_act1_arithmitics;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_act1_arithmitics {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
