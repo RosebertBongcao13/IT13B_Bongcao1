c8fd99484b5f66a466f3bcbfeb2e74d6590ba6d4
RosebertBongcao13 <rosebertcolegio143@gmail.com>
2025-02-13 17:12:42 +0800

    deleted and added comment

--- Bongcao_IT13B_Activities/src/main/java/Prelim/Lab_act1_Arithmetics.java
+++ Bongcao_IT13B_Activities/src/main/java/Prelim/Lab_act1_Arithmetics.java
@@ -4,14 +4,14 @@ public class Lab_act1_Arithmetics {
 
     public static void main(String[] args) {
 
-        int x = 10; // First number
-        int y = 4;  // Second number
-        int z = 6;  // Third number
+        int x = 10; 
+        int y = 4;  
+        int z = 6;  
 
-        // Corrected output statements
-        System.out.println(x + "*" + y + "+" + z + "-46"); // 10*4+6-46
-        System.out.println("(" + x + "-" + y + ")%" + z + "=0"); // (10-4)%6=0
-        System.out.println("(" + x + "+" + y + "+" + z + ")/3=" + ((x + y + z) / 3)); // (10+4+6)/3=6
-        System.out.println(x + "*" + z + "-(" + y + "*" + y + ")=" + (x * z - (y * y))); // 10*6-(4*4)=44
+        // Permi error tabanggg
+        System.out.println(x + "*" + y + "+" + z + "-46"); 
+        System.out.println("(" + x + "-" + y + ")%" + z + "=0"); 
+        System.out.println("(" + x + "+" + y + "+" + z + ")/3=" + ((x + y + z) / 3)); 
+        System.out.println(x + "*" + z + "-(" + y + "*" + y + ")=" + (x * z - (y * y))); 
     }
 }
