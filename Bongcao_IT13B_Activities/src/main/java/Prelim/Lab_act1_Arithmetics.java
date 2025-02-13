1bc8f224cfb5b06642c387dcacab2e7200d42945
Cheram Rodriguez <cheramrodriguez724@gmail.com>
2025-02-13 11:42:07 +0800

    Added final code

--- Bongcao_IT13B_Activities/src/main/java/Prelim/Lab_act1_Arithmetics.java
+++ Bongcao_IT13B_Activities/src/main/java/Prelim/Lab_act1_Arithmetics.java
@@ -2,12 +2,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package Prelim;
 
 /**
  *
  * @author User
  */
+
 public class Lab_act1_Arithmetics {
     
+    public static void main(String [] args){
+        
+        int x = 10;
+        int y = 4;
+        int z = 6;
+        
+        System.out.println("x" + "y" + "+" + "x" + "=" + (x * y + z));
+        System.out.println("(x" + "-" + "y)" + "%" + "z" + "=" + (x-y) %z);
+        System.out.println("(x" + "+" + "y)" + "+" +"z)" + "/" + "3" + "=" + (x+y+z) /3);
+        System.out.println("x" + "x" + "z" + "-" + "(y" + "x" + "y" + "=" + (x * z - (y * y)));
+        
+    }
 }
