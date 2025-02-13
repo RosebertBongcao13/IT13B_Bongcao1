3238aff9e7d3cb4309336d5313a4ba699538308c
Cheram Rodriguez <cheramrodriguez724@gmail.com>
2025-02-13 11:50:10 +0800

    added

--- Bongcao_IT13B_Activities/src/main/java/Prelim/Lab_act1_Arithmetics.java
+++ Bongcao_IT13B_Activities/src/main/java/Prelim/Lab_act1_Arithmetics.java
@@ -18,10 +18,10 @@ public class Lab_act1_Arithmetics {
         int y = 4;
         int z = 6;
         
-        System.out.println("x" + "y" + "+" + "x" + "=" + (x * y + z));
-        System.out.println("(x" + "-" + "y)" + "%" + "z" + "=" + (x-y) %z);
-        System.out.println("(x" + "+" + "y)" + "+" +"z)" + "/" + "3" + "=" + (x+y+z) /3);
-        System.out.println("x" + "x" + "z" + "-" + "(y" + "x" + "y" + "=" + (x * z - (y * y)));
+        System.out.println("10" + "y" + "+" + "x" + "=" + (x * y + z));
+        System.out.println("(10" + "-" + "y)" + "%" + "z" + "=" + (x-y) %z);
+        System.out.println("(10" + "+" + "y)" + "+" +"z)" + "/" + "3" + "=" + (x+y+z) /3);
+        System.out.println("10" + "x" + "z" + "-" + "(y" + "x" + "y" + "=" + (x * z - (y * y)));
         
     }
 }
