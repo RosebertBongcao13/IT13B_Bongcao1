6d700ca6d71e3a6d5c840218df4c9fb5186ab169
Mrgumball2004 <rosebertcolegio143@gmail.com>
2025-03-24 11:29:05 +0800

    midterm Array

---
+++ Bongcao_IT13B_Activities/src/main/java/Midterm/Bongcao_Single_Array_Number_1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package midterm;
+/**
+ *
+ * @author User
+ */
+public class Bongcao_Single_Array_Number_1 {   
+    
+     //char letters[] = {'a', 'b', 'c', 'D', 'e', 'z', '9'};
+       //System.out.print(a + " , ");System.out.print(a + " , ");System.out.print(a + " , ");    
+    
+    public static void main (String[] args){
+        char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        
+    
+        for(char a : letters){
+            System.out.print(a + " , ");
+        }
+        
+    }
+}
